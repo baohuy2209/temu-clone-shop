5a12bac3d9a3e7fba7117b67e30ff583d6b08e31
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2026-05-14 17:33:26 +0700

    update unami

--- lib/umami.ts
+++ lib/umami.ts
@@ -1,7 +1,7 @@
 import umami from "@umami/node";
 
 umami.init({
-  websiteId: "(YOUR UMAMI WEBSITE ID)", // Your website id
+  websiteId: "88093007-ccaf-45aa-9190-45904651d254", // Your website id
   hostUrl: "https://cloud.umami.is", // URL to your Umami instance
 });
 
