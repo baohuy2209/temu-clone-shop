dc4cd9d778732936cccf339c686dcbef84b97c8c
baohuy2209 <104624325+baohuy2209@users.noreply.github.com>
2026-05-14 18:43:41 +0700

    update

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "next-sanity";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
-
+export const runtime = "nodejs";
 export async function POST(req: Request) {
   // Get Stripe client
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
